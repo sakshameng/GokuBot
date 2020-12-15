03311a209d86d96bb5fa33f6aa004a24aa085838
Saksham Singh <68587810+sakshameng@users.noreply.github.com>
2020-12-15 05:56:58 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import re
 from sys import argv
 from typing import Optional
 
-from GokusBot import (ALLOW_EXCL, CERT_PATH, DONATION_LINK, LOGGER,
+from GokuBot import (ALLOW_EXCL, CERT_PATH, DONATION_LINK, LOGGER,
                           OWNER_ID, PORT, SUPPORT_CHAT, TOKEN, URL, WEBHOOK,
                           SUPPORT_CHAT, dispatcher, StartTime, telethn, updater)
 # needed to dynamically load modules
